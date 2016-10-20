34ccf509da54226ec570ab310881b40762ad4c62
Hasina6 <raffiudeen9994890057@gmail.com>
2016-10-20 14:34:47 +0530

    Update pro5.c

--- pro5.c
+++ pro5.c
@@ -3,13 +3,10 @@
 int main()
 {
 char c;
-int a2,b3;
 clrscr();
 printf("Enter the alphabet ");
 scanf("%c",&c);
-a2=(c==a||c==e||c==i||c==o||c==u);
-b3=(c=A||c==E||c==I||c==O||c==U);
-if(a2||b3)
+if((c==a||c==e||c==i||c==o||c==u)||(c==A||c==E||c==I||c==O||c==U))
 printf("Vowel character");
 else
 printf("Consonant character");
